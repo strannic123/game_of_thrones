8760c2059d5e75791afa1d3561f5d1c1c3817ae7
strannic123 <manager350@mail.ru>
2020-08-19 20:28:31 +0300

    add button show/hide Character

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import ErrorMessage from "../errorMessage";
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+export default class App extends Component {
+
+    state = {
+        showRandomChar: true,
+        error: false
+    }
+
+    onShowHideRandomChar = () => {
+        return (
+            this.setState((state) => {
+              return  {showRandomChar: !state.showRandomChar}
+            })
+        )
+        }
+
+
+    render() {
+        if (this.state.error) {
+            return <ErrorMessage/>
+        }
+
+        const {showRandomChar} = this.state
+
+        const btnHideShow = showRandomChar ? <RandomChar/> : null;
+
+        return (
+            <>
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {/*<RandomChar/>*/}
+                            {btnHideShow}
+                            <button
+                                className="btn-primary mb-5 p-2 rounded"
+                                onClick={this.onShowHideRandomChar}
+                            >Toggle random character</button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+
 };
 
-export default App;
